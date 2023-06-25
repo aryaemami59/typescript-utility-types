e380af9cb96943503294bd1e25874ebc5275cf99
Arya Emami <aryaemami59@yahoo.com>
2023-06-25 06:35:35 -0500

    added TupleOfRangedLength

--- script.ts
+++ script.ts
@@ -5,14 +5,17 @@ export type AnyTuple = readonly [...unknown[]];
 export type AnyArray = unknown[];
 /** extends from {@link Function} */
 export type AnyFunction = (...args: unknown[]) => unknown;
+
+export type AnyNonNullishValue = NonNullable<unknown>;
+/** Can be used to generate intellisense for types that are a union of some strings and any string */
+export type AnyString = string & AnyNonNullishValue;
+
 /** extends from {@link AnyObject} */
 export type EmptyObject = Record<string, never>;
 /** This array has access to mutable methods like `push` and `pop` but it cannot use them. */
 export type MutableEmptyArray = never[];
 /** This array does not have access to mutable methods like `push` and `pop`. */
 export type ImmutableEmptyArray = readonly never[];
-
-export type AnyNonNullishValue = NonNullable<unknown>;
 
 type Enumerate<
   N extends number,
@@ -44,7 +47,12 @@ type TupleOf<
   : TReadonly extends true
   ? Readonly<TupleOf<T, N, readonly [T, ...Readonly<U>], TReadonly>>
   : TupleOf<T, N, [T, ...U], TReadonly>;
-/** Tuple of specific length. Set R to true to make it readonly. N cannot be a negative number. */
+/**
+ * Tuple of specific length.
+ * T can be a union type.
+ * Set R to true to make it readonly.
+ * N cannot be a negative number.
+ */
 export type Tuple<
   T,
   N extends number,
@@ -56,6 +64,36 @@ export type Tuple<
     ? Readonly<TupleOf<T, N, [], TReadonly>>
     : TupleOf<T, N, [], TReadonly>
   : never;
+
+export type TupleUnion<TTuple extends AnyTuple> = TTuple extends [
+  ...[...infer R]
+]
+  ? Tuple<R[IndexOf<R>], TTuple["length"]>
+  : never;
+
+export type TupleOfMinLength<
+  T,
+  TMin extends number
+> = TMin extends AssertPositive<TMin> ? [...Tuple<T, TMin>, ...T[]] : never;
+
+export type TupleOfMaxLength<
+  T,
+  TMax extends number
+> = TMax extends AssertPositive<TMax> ? Partial<[...Tuple<T, TMax>]> : never;
+
+export type TupleOfRangedLength<
+  T,
+  TMin extends number,
+  TMax extends number
+> = Range<TMax, TMin> extends number ? Tuple<T, Range<TMax, TMin>> : never;
+
+// const element: TupleUnion<[Record<"a", unknown>, "a", "b"]> = ["a", "a", "b"];
+const element: TupleOfRangedLength<Record<"a", unknown> | "a", 1, 4> = [
+  "a",
+  "a",
+  "a",
+  "a",
+];
 
 /** Checks if T extends from U. Checks if T is assignable to U. */
 export type ExtendsFrom<T, U> = T extends U ? true : false;
@@ -81,10 +119,28 @@ export type RecursiveMutable<T> = {
 };
 
 export type Mutable<T> = { -readonly [P in keyof T]: T[P] };
-
+/** Recursive {@link Readonly} */
 export type RecursiveImmutable<T> = {
   +readonly [P in keyof T]: RecursiveImmutable<T[P]>;
 };
+
+export type RecursivePartial<T> = {
+  [P in keyof T]?: RecursivePartial<T[P]>;
+};
+
+export type RecursiveRequired<T> = {
+  [P in keyof T]-?: RecursiveRequired<T[P]>;
+};
+
+export type LowerOrUpperCase<T extends string> = Lowercase<T> | Uppercase<T>;
+
+export type AnyCase<T extends string> = string extends T
+  ? string
+  : T extends `${infer F1}${infer F2}${infer R}`
+  ? `${LowerOrUpperCase<F1>}${LowerOrUpperCase<F2>}${AnyCase<R>}`
+  : T extends `${infer F}${infer R}`
+  ? `${LowerOrUpperCase<F>}${AnyCase<R>}`
+  : "";
 
 export type IsReadOnly<T> = Readonly<T> extends T ? true : false;
 /** Get a union type of the indices of an array. */

29136815679672ca81bf392abb44863437c2c7df
Arya Emami <aryaemami59@yahoo.com>
2023-06-26 21:05:42 -0500

    fixed tuples

---
+++ index.d.ts
@@ -0,0 +1,274 @@
+export type AnyObject = Record<string, unknown>;
+/**
+ * Does not have access to mutable methods like `push` and `pop`.
+ *
+ * same as {@link ReadonlyArray<unknown>}
+ */
+export type AnyImmutableArray = readonly unknown[];
+/**
+ * has access to mutable methods like `push` and `pop`.
+ *
+ * extends from {@link AnyImmutableArray}
+ *
+ * same as {@link Array<unknown>}
+ */
+export type AnyMutableArray = unknown[];
+/**
+ * extends from {@link Function}
+ */
+export type AnyFunction = (...args: unknown[]) => unknown;
+/**
+ * same as `{}`
+ */
+export type AnyNonNullishValue = NonNullable<unknown>;
+/**
+ * Can be used to generate intellisense for types that are a union of some strings and any string.
+ *
+ * @example
+ * ```
+ * const example: AnyString | "specific string" = "specific string";
+ * const example: AnyString | "specific string" = "any other string";
+ * It accepts any other string but also gives "specific string" in intellisense.
+ * ```
+ */
+export type AnyString = string & AnyNonNullishValue;
+/**
+ * Can be used to generate intellisense for types that are a union of some numbers and any number.
+ *
+ * works similar to {@link AnyString}
+ */
+export type AnyNumber = number & AnyNonNullishValue;
+/**
+ * extends from {@link AnyObject}
+ */
+export type EmptyObject = Record<string, never>;
+/**
+ * This array has access to mutable methods like `push` and `pop` but it cannot use them.
+ *
+ * extends from {@link AnyMutableArray}
+ */
+export type EmptyMutableArray = never[];
+/**
+ * This array does not have access to mutable methods like `push` and `pop`.
+ *
+ * extends from {@link AnyImmutableArray}
+ */
+export type EmptyImmutableArray = readonly never[];
+/**
+ * extends from {@link EmptyMutableArray}
+ *
+ * same as
+ * ```
+ * Tuple<never, 0>
+ * or:
+ * Tuple<unknown, 0>
+ * ```
+ */
+export type EmptyMutableTuple = [];
+/**
+ * extends from {@link EmptyImmutableArray}
+ *
+ * same as
+ * ```
+ * Tuple<never, 0, true>
+ * or:
+ * Tuple<unknown, 0, true>
+ * ```
+ */
+export type EmptyImmutableTuple = readonly [];
+
+type Enumerate<
+  TNumber extends number,
+  Acc extends number[] = []
+> = TNumber extends AssertPositive<TNumber>
+  ? Acc["length"] extends TNumber
+    ? Acc[number]
+    : Enumerate<TNumber, [Acc["length"], ...Acc]>
+  : never;
+/**
+ * Specify exclusive range of numbers starting from 0.
+ *
+ * TMax cannot be higher than 999.
+ *
+ * TMin is the starting value, it cannot be a negative number.
+ */
+export type ExclusiveRange<
+  TMax extends number,
+  TMin extends number = 0
+> = TMin extends AssertPositive<TMin>
+  ? Exclude<Enumerate<TMax>, Enumerate<TMin>>
+  : never;
+/**
+ * Checks if a number is positive. TNumber could be a union type but must be a literal number type.
+ */
+export type AssertPositive<TNumber extends number> = number extends TNumber
+  ? never
+  : `${TNumber}` extends `-${string}`
+  ? never
+  : TNumber;
+/**
+ * Checks if a number is an integer. TNumber could be a union type but must be a literal number type.
+ */
+export type AssertInteger<TNumber extends number> = number extends TNumber
+  ? TNumber
+  : `${TNumber}` extends `${string}.${string}`
+  ? never
+  : TNumber;
+
+type TupleOf<
+  TElement,
+  TLength extends number,
+  TArray extends readonly unknown[],
+  TReadonly extends boolean = false
+> = TArray["length"] extends TLength
+  ? TArray
+  : TReadonly extends true
+  ? Readonly<
+      TupleOf<
+        TElement,
+        TLength,
+        readonly [TElement, ...Readonly<TArray>],
+        TReadonly
+      >
+    >
+  : TupleOf<TElement, TLength, [TElement, ...TArray], TReadonly>;
+/**
+ * Tuple of specific length.
+ *
+ * TElement can be a union type.
+ *
+ * Set TReadonly to true to make it readonly.
+ *
+ * TLength cannot be a negative number. It can also be a union which will return union of tuples.
+ * @example
+ * ```
+ * const example: Tuple<string, 1 | 2> = ["a"];
+ * const example: Tuple<string, 1 | 2> = ["a", "b"];
+ * const example: Tuple<string, 1 | 2, true> = ["a", "b"];
+ * const example: Tuple<string, 1 | 2> = ["a", "b"] as const; // Results in an error.
+ * ```
+ */
+export type Tuple<
+  TElement,
+  TLength extends number,
+  TReadonly extends boolean = false
+> = TLength extends AssertPositive<TLength>
+  ? number extends TLength
+    ? readonly TElement[]
+    : TReadonly extends true
+    ? TupleOf<TElement, TLength, readonly [], TReadonly>
+    : TupleOf<TElement, TLength, [], TReadonly>
+  : never;
+
+/**
+ * Returns a tuple where the elements can be repeated and the order of the elements will not matter.
+ *
+ * The length of the tuple however cannot change.
+ * @example
+ * ```
+ * const example: TupleUnion<["a", 2]> = ["a", 2];
+ * const example: TupleUnion<["a", 2]> = [2, "a"];
+ * const example: TupleUnion<["a", 2]> = ["a", "a"];
+ * const example: TupleUnion<["a", 2]> = [2, 2];
+ * const example: TupleUnion<readonly ["a", 2]> = [2, 2] as const;
+ * const example: TupleUnion<readonly ["a", 2]> = [2, 2];
+ * const example: TupleUnion<["a", 2]> = [2, 2, 2]; // Results in an error.
+ * const example: TupleUnion<["a", 2]> = [2]; // Results in an error.
+ * const example: TupleUnion<["a", 2]> = [2, 2] as const; // Results in an error since the TTuple is not readonly the resulting tuple cannot be readonly either.
+ * ```
+ */
+export type TupleUnion<TTuple extends AnyImmutableArray> =
+  TTuple extends readonly [...[...infer R]]
+    ? Tuple<R[IndexOf<R>], TTuple["length"], IsReadOnly<TTuple>>
+    : never;
+
+export type Length<T extends readonly unknown[]> = T["length"];
+
+export type TupleOfMinLength<
+  T,
+  TMin extends number
+> = TMin extends AssertPositive<TMin> ? [...Tuple<T, TMin>, ...T[]] : never;
+
+export type TupleOfMaxLength<
+  T,
+  TMax extends number
+> = TMax extends AssertPositive<TMax> ? Partial<[...Tuple<T, TMax>]> : never;
+/**
+ * Gives tuple of exclusive range.
+ *
+ * TElement is the type of element in the tuple. It can be a union type.
+ *
+ * TMax cannot be less than or equal to TMin.
+ */
+export type TupleOfRangedLength<
+  TElement,
+  TMin extends number,
+  TMax extends number
+> = ExclusiveRange<TMax, TMin> extends number
+  ? Tuple<TElement, ExclusiveRange<TMax, TMin>>
+  : never;
+
+/**
+ * Checks if T extends from U. Checks if T is assignable to U.
+ */
+export type ExtendsFrom<T, U> = T extends U ? true : false;
+
+export type Composite = AnyMutableArray | AnyFunction | AnyObject;
+
+export type ObjectOrArray = AnyMutableArray | AnyObject;
+/**
+ * Checks if 2 types are equal.
+ */
+export type Equals<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
+  T
+>() => T extends Y ? 1 : 2
+  ? true
+  : false;
+
+type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
+/**
+ * Exclusive OR
+ */
+export type XOR<T, U> = T | U extends object
+  ? (T & Without<U, T>) | (U & Without<T, U>)
+  : T | U;
+
+export type RecursiveMutable<T> = {
+  -readonly [P in keyof T]: RecursiveMutable<T[P]>;
+};
+
+export type Mutable<T> = { -readonly [P in keyof T]: T[P] };
+/**
+ * Recursive {@link Readonly}
+ */
+export type RecursiveImmutable<T> = {
+  +readonly [P in keyof T]: RecursiveImmutable<T[P]>;
+};
+
+export type RecursivePartial<T> = {
+  [P in keyof T]?: RecursivePartial<T[P]>;
+};
+
+export type RecursiveRequired<T> = {
+  [P in keyof T]-?: RecursiveRequired<T[P]>;
+};
+
+export type LowerOrUpperCase<T extends string> = Lowercase<T> | Uppercase<T>;
+/**
+ * Use with caution. Does not scale well.
+ */
+export type AnyCase<T extends string> = string extends T
+  ? string
+  : T extends `${infer F1}${infer F2}${infer R}`
+  ? `${LowerOrUpperCase<F1>}${LowerOrUpperCase<F2>}${AnyCase<R>}`
+  : T extends `${infer F}${infer R}`
+  ? `${LowerOrUpperCase<F>}${AnyCase<R>}`
+  : "";
+/**
+ * Checks to see if something is readonly.
+ */
+export type IsReadOnly<T> = Readonly<T> extends T ? true : false;
+/**
+ * Get a union type of the indices of an array.
+ */
+export type IndexOf<T extends AnyImmutableArray> = ExclusiveRange<T["length"]>;
---
+++ index.test-d.ts
@@ -0,0 +1,101 @@
+import {
+  expectAssignable,
+  expectNotAssignable,
+  expectNotType,
+  expectType,
+  printType,
+} from "tsd";
+import {
+  AnyFunction,
+  AnyImmutableArray,
+  AnyMutableArray,
+  AnyNonNullishValue,
+  AnyObject,
+  AssertInteger,
+  AssertPositive,
+  EmptyImmutableArray,
+  EmptyImmutableTuple,
+  EmptyMutableArray,
+  EmptyMutableTuple,
+  EmptyObject,
+  Equals,
+  ExclusiveRange,
+  ExtendsFrom,
+  IndexOf,
+  Length,
+  Tuple,
+  TupleOfRangedLength,
+  TupleUnion,
+  XOR,
+} from ".";
+
+// TupleUnion
+expectAssignable<TupleUnion<["a", 2]>>(["a", 2]);
+expectAssignable<TupleUnion<["a", 2]>>(["a", 2]);
+expectAssignable<TupleUnion<["a", 2]>>([2, "a"]);
+expectAssignable<TupleUnion<["a", 2]>>(["a", "a"]);
+expectAssignable<TupleUnion<["a", 2]>>([2, 2]);
+expectAssignable<TupleUnion<readonly ["a", 2]>>([2, 2] as const);
+expectAssignable<TupleUnion<readonly ["a", 2]>>([2, 2]);
+expectNotType<TupleUnion<["a", 2]>>([2, 2, 2]);
+expectNotType<TupleUnion<["a", 2]>>([2]);
+expectNotType<TupleUnion<["a", 2]>>([2, 2] as const);
+// Tuple
+expectAssignable<Tuple<string, 1 | 2>>(["a"]);
+expectAssignable<Tuple<string, 1 | 2>>(["a", "b"]);
+expectAssignable<Tuple<string, 1 | 2, true>>(["a", "b"]);
+expectNotType<Tuple<string, 1 | 2>>(["a", "b"] as const);
+// TupleOfRangedLength
+expectAssignable<TupleOfRangedLength<"a" | "b", 1, 3>>(["a"]);
+// Length
+expectType<Length<["a"]>>(1);
+expectType<Length<[]>>(0);
+// Equals
+expectType<Equals<EmptyImmutableTuple, Tuple<never, 0, true>>>(true);
+expectType<Equals<EmptyImmutableTuple, Tuple<unknown, 0, true>>>(true);
+expectType<Equals<EmptyMutableTuple, Tuple<never, 0>>>(true);
+expectType<Equals<EmptyMutableTuple, Tuple<unknown, 0>>>(true);
+expectType<Equals<AnyMutableArray, Array<unknown>>>(true);
+expectType<Equals<AnyImmutableArray, ReadonlyArray<unknown>>>(true);
+expectType<Equals<AnyNonNullishValue, {}>>(true);
+// ExtendsFrom
+expectType<ExtendsFrom<EmptyImmutableTuple, EmptyImmutableArray>>(true);
+expectType<ExtendsFrom<EmptyMutableTuple, EmptyMutableArray>>(true);
+expectType<ExtendsFrom<EmptyImmutableArray, AnyImmutableArray>>(true);
+expectType<ExtendsFrom<EmptyMutableArray, AnyMutableArray>>(true);
+expectType<ExtendsFrom<EmptyObject, AnyObject>>(true);
+expectType<ExtendsFrom<AnyFunction, Function>>(true);
+expectType<ExtendsFrom<AnyMutableArray, AnyImmutableArray>>(true);
+// Range
+expectAssignable<ExclusiveRange<3>>(2);
+expectAssignable<ExclusiveRange<3, 1>>(2);
+expectAssignable<ExclusiveRange<999, 1>>(2);
+expectNotType<ExclusiveRange<3, -1>>(2);
+expectNotType<ExclusiveRange<3, 4>>(2);
+declare const a: ExclusiveRange<3, 1>;
+printType(a);
+// TupleOfRangedLength
+expectAssignable<TupleOfRangedLength<"a", 1, 3>>(["a"]);
+expectAssignable<TupleOfRangedLength<"a", 1, 3>>(["a", "a"]);
+declare const b: TupleOfRangedLength<"a" | "b", 1, 3>;
+printType(b);
+expectAssignable<TupleOfRangedLength<"a" | "b", 1, 3>>(["a", "b"]);
+expectNotType<TupleOfRangedLength<"a", 1, 3>>(["a", "a", "a"]);
+// IndexOf
+expectAssignable<IndexOf<["a", "b", "c"]>>(1);
+expectAssignable<IndexOf<readonly ["a", "b", "c"]>>(2);
+// XOR
+expectAssignable<XOR<{ a: "a" }, { b: "b" }>>({ a: "a" });
+expectAssignable<XOR<{ a: "a" }, { b: "b" }>>({ b: "b" });
+expectNotType<XOR<{ a: "a" }, { b: "b" }>>({ b: "b", a: "a" });
+// AssertPositive
+expectType<AssertPositive<1>>(1);
+expectAssignable<AssertPositive<1 | 2>>(1);
+expectNotType<AssertPositive<number>>(1);
+expectNotType<AssertPositive<-1>>(1);
+// AssertInteger
+expectType<AssertInteger<1>>(1);
+expectType<AssertInteger<-1>>(-1);
+expectAssignable<AssertInteger<1 | 2>>(1);
+expectNotType<AssertInteger<1.1>>(1);
+expectNotAssignable<AssertInteger<1.1 | 2.2>>(1);
